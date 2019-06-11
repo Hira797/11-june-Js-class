e17360e2be573e69f456319c2747878a515e9abc
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-06-11 18:00:28 +0500

    Add files via upload

--- my.js
+++ my.js
@@ -7,3 +7,14 @@ function chooseLight(){
     bodyElement.classList.remove("dark");
     bodyElement.classList.add("light");
 }
+if (new Date().getHours() >= 17 && new Date().getMinutes() >= 57) {
+    chooseDark();
+} 
+else {
+    chooseLight();
+}
+// agr 20 baj chukay hain toh chooseDark() warna chooseLight()
+// agr hours 17 ya us se ziada AND minutes 50 ya us se ziada
+//  baj chuke hain toh chooseDark() warna chooseLight() 
+// newDate().getHours >=
+// new Date().getMinutes

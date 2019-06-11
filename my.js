bd5d4e9266536f5fb828acdf0acf095c1e4f48ab
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-06-11 15:34:02 +0500

    Add files via upload

---
+++ my.js
@@ -0,0 +1,10 @@
+console.log(){}
+
+function  chooseDark(){
+    document.querySelector("body").classList.remove("light");
+    document.querySelector("body").classList.remove("dark");
+}
+function  chooseLight(){
+    document.querySelector("body").classList.remove("dark");
+    document.querySelector("body").classList.remove("light");
+}
